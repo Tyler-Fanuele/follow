2856f11c0f39b91e077fc85f683f08e8323d9552
Tyler-Fanuele <tfanuele@gmail.com>
2023-07-01 03:15:07 -0400

    Added command and command file system and implmented logging system

--- follow.py
+++ follow.py
@@ -8,6 +8,38 @@ from watchdog.observers import Observer  #creating an instance of the watchdog.o
 from watchdog.events import LoggingEventHandler  #implementing a subclass of watchdog.events.FileSystemEventHandler which is LoggingEventHandler in our case
 from watchdog.events import FileModifiedEvent
 import subprocess
+from datetime import datetime
+
+# Create argument parser object 
+parser = argparse.ArgumentParser(
+                    prog='Follow',
+                    description='A program that follows file changes and executes a command or set of commands when those changes occur',
+                    epilog='Epilog') 
+
+# Add location argument to specify what file or dir to follow. If blank current dir is used
+parser.add_argument('location', help="The location to be followed, can be dir or file", nargs='?', default=os.getcwd())
+
+# Add exec argument to specify what command of file containing multiple commands to be executed during execution period
+parser.add_argument('-e', '--exec', nargs='?', help="The commands or command to be executed. Can be string, path to file, or nothing. Nothing will default to printing changes")
+
+# Add timing argument to specify how long to wait to check if command execution is needed, default is 20
+parser.add_argument('-t', '--timing', nargs='?', default=20, type=float, help="Time in between checking for execution, default 20 seconds")
+
+parser.add_argument('--verbose', action='store_true', default=False, help="Sets flag enabling verbose")
+ 
+# Parse the arguments
+args = parser.parse_args()  
+
+def log(log_string, log_type='log'):
+    if log_type == 'error':
+        print(datetime.now() , " ERROR: " , log_string)
+    elif log_type == 'verbose' and args.verbose == True:
+        print(datetime.now() , " VERBOSE: " , log_string)
+    elif log_type == 'log':
+        print(log_string)
+
+
+
 
 update_events = 0
 update_list = list()
@@ -15,7 +47,7 @@ update_list = list()
 class FileEvent(FileModifiedEvent):
     def dispatch(self, event):
         if event.is_directory == False and event.event_type  == 'modified':
-            #print(event)
+            log("File Event Seen: " + str(event), 'verbose')
             global update_events, update_list
             update_events = update_events + 1
             update_list.append(event)
@@ -23,7 +55,7 @@ class FileEvent(FileModifiedEvent):
 class DirEvent(FileModifiedEvent):
     def dispatch(self, event):
         if event.src_path not in ignore_list:
-            #print(event)
+            log("Dir Event Seen: " + str(event), 'verbose')
             global update_events, update_list
             update_events = update_events + 1
             update_list.append(event)
@@ -48,29 +80,6 @@ if os.path.isfile(os.path.join(os.path.curdir , ".followignore")):
                 for file in files:
                     additional_ignored.append(os.path.abspath(os.path.join(dirpath, file)))
     ignore_list = ignore_list + additional_ignored
-                    
-#print("Ignore List: ", ignore_list)
-
-
-
-# Create argument parser object 
-parser = argparse.ArgumentParser(
-                    prog='Follow',
-                    description='A program that follows file changes and executes a command or set of commands when those changes occur',
-                    epilog='Epilog') 
-
-# Add location argument to specify what file or dir to follow. If blank current dir is used
-parser.add_argument('location', help="The location to be followed, can be dir or file", nargs='?', default=os.getcwd())
-
-# Add exec argument to specify what command of file containing multiple commands to be executed during execution period
-parser.add_argument('-e', '--exec', nargs='?', help="The commands or command to be executed. Can be string, path to file, or nothing. Nothing will default to printing changes")
-
-# Add timing argument to specify how long to wait to check if command execution is needed, default is 20
-parser.add_argument('-t', '--timing', nargs='?', default=20, type=float, help="Time in between checking for execution, default 20 seconds")
- 
-# Parse the arguments
-args = parser.parse_args() 
-print(args)
 
 # Bool true if location specified is dir
 location_is_dir = False 
@@ -84,21 +93,24 @@ if execution_provided == True:
         exec_file = open(args.exec)
         for line in exec_file:
             exec_list.append(line.strip())
-else:
-    exec_list.append(args.exec)
- 
-print("Location: " , args.location)
+    else:
+        exec_list.append(args.exec)
+
+log("Command List: " + str(exec_list), 'verbose')
+
+log("Follow Location: " + args.location, "verbose")
 # Check if location path exists 
-if os.path.exists(args.location):
-    print(args.location , " exists") 
+if os.path.exists(args.location): 
+    log(args.location + " exists", 'verbose') 
     # If location path exists check if its a dir
     if os.path.isdir(args.location):
         location_is_dir = True
 else:
     # If location path doesn't exist terminate program
-    print(args.location + " does not exist, exiting program")
-    exit()
-print(args.location + " is a dir" if location_is_dir else args.location + " is a file")
+    log("Location:'" + args.location + "' does not exist, exiting program", 'error')
+    exit(1)
+
+
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s')
@@ -110,24 +122,32 @@ if execution_provided == True:
         event_handler = FileEvent(args.location)
 else:
     event_handler = LoggingEventHandler() 
+
+log("Starting follow process\n:")
+log(args.location + " is a valid directory" if location_is_dir else args.location + " is a valid file")
+log("Commands to run when update is registered:")
+for command in exec_list:
+    log("   " + str(command))
 observer = Observer()
 observer.schedule(event_handler, args.location, recursive=True)  #Scheduling monitoring of a path with the observer instance and event handler. There is 'recursive=True' because only with it enabled, watchdog.observers.Observer can monitor sub-directories
 observer.start()  #for starting the observer thread
-print("Keyboard interrupt to end program")
+log("Use keyboard interrupt to exit program")
 try:
     while True: 
         time.sleep(args.timing)
+        # If a execution file or string was provided and there was a update event
         if execution_provided == True and update_events != 0:
-            print("exec")
+            # Loop through commands and run them, waiting for return before running the next command
             for command in exec_list:
                 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
                 process.wait()
-                print(process.returncode)
+                log("Ran command: '" + str(command) + "' with return value of: " + str(process.returncode))
+            # Reset the update events variables for next check
             update_events = 0
             update_list = list()
         else:
-            print("dont exec") 
+            log("No updates detected, no execution of command", 'verbose')
         
 except KeyboardInterrupt:
-    observer.stop()
+    observer.stop() 
 observer.join()  
